0ed76f095037c7f936d19e2a8bc3d79786c31c18
amengpp <amengppppp@163.com>
2023-05-09 19:53:40 +0800

    a3

--- functions/queryopenai.js
+++ functions/queryopenai.js
@@ -1,59 +1,61 @@
 const fetch = require('isomorphic-fetch');
-const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
-  const picked = new Headers();
-  for (const key of headers.keys()) {
-    if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
-      const value = headers.get(key);
-      if (typeof value === "string") {
-        picked.set(key, value);
-      }
-    }
-  }
-  return picked;
-};
-
-const CORS_HEADERS: Record<string, string> = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "Content-Type, Authorization",
-};
-
-// exports.handler = async (req) =>  {
-//   console.log(req)
-//   if (req.method === "OPTIONS") {
-//     return new Response(null, {
-//       headers: CORS_HEADERS,
-//     });
+// const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
+//   const picked = new Headers();
+//   for (const key of headers.keys()) {
+//     if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
+//       const value = headers.get(key);
+//       if (typeof value === "string") {
+//         picked.set(key, value);
+//       }
+//     }
 //   }
+//   return picked;
+// };
 //
-//   const { pathname, search } = req.nextUrl ? req.nextUrl : new URL(req.url);
-//   const url = new URL(pathname + search, "https://api.openai.com").href;
-//   const headers = pickHeaders(req.headers, ["content-type", "authorization"]);
-//
-//   const res = await fetch(url, {
-//     body: req.body,
-//     method: req.method,
-//     headers,
-//   });
-//
-//   const resHeaders = {
-//     ...CORS_HEADERS,
-//     ...Object.fromEntries(
-//         pickHeaders(res.headers, ["content-type", /^x-ratelimit-/, /^openai-/])
-//     ),
-//   };
-//
-//   return new Response(res.body, {
-//     headers: resHeaders,
-//   });
-// }
+// const CORS_HEADERS: Record<string, string> = {
+//   "access-control-allow-origin": "*",
+//   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+//   "access-control-allow-headers": "Content-Type, Authorization",
+// };
+
 exports.handler = async (event) => {
   console.log(event)
-  return {
-    statusCode: 200,
-    body: "test ok",
-  };
+  // if (event.httpMethod === "OPTIONS") {
+  //   return new Response(null, {
+  //     headers: CORS_HEADERS,
+  //   });
+  // }
+  // console.log(event.httpMethod)
+
+  //
+  // const url = new URL(event.path +"?"+event.rawQuery, "https://api.openai.com").href;
+  // console.log(url)
+  // const headers = pickHeaders(event.headers, ["content-type", "authorization"]);
+
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    body: event.body,
+    method: event.httpMethod,
+    event.headers,
+  });
+  return new Response(res.body, {
+    headers: res.headers,
+  });
 }
+// exports.handler = async (event) => {
+//   console.log(event)
+//   const response = await fetch(
+//     'https://api.openai.com/v1/completions',
+//     requestOptions
+//   );
+//
+//   const data = await response.json();
+//   console.log('🚀 file: queryopenai.js:41 ~ exports.handler= ~ data:', data);
+//
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ result: data.choices[0].text || {} }),
+//   };
+// }
 // exports.handler = async (event) => {
 //   const { newMessage, messageHistory } = JSON.parse(event.body);
 //
